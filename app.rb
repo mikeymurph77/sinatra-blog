68316c9c44ddc81e5c48ab166a6f8e6c8d21993e
Michael Murphy <mikeymurph77@gmail.com>
2016-01-09 15:44:03 -0500

    Can edit posts

--- app.rb
+++ app.rb
@@ -12,34 +12,6 @@ class Post < ActiveRecord::Base
   validates :body, presence: true
 end
 
-# Routes
-get "/" do
-  @posts = Post.order("created_at DESC")
-  @title = "Welcome."
-  erb :"posts/index"
-end
-
-get "/posts/create" do
-  @title = "Create post"
-  @post = Post.new
-  erb :"posts/create"
-end
-
-post "/posts" do
- @post = Post.new(params[:post])
- if @post.save
-   redirect "posts/#{@post.id}", :notice => 'Congrats! Love the new post. (This message will disapear in 4 seconds.)'
- else
-   redirect "posts/create", :error => 'Something went wrong. Try again. (This message will disapear in 4 seconds.)'
- end
-end
-
-get "/posts/:id" do
-  @post = Post.find(params[:id])
-  @title = @post.title
-  erb :"posts/view"
-end
-
 # Helpers
 helpers do
   def title
@@ -49,4 +21,46 @@ helpers do
       "Welcome."
     end
   end
+end
+
+# Routes
+# Get all posts
+get "/" do
+  @posts = Post.order("created_at DESC")
+  @title = "Welcome."
+  erb :"posts/index"
+end
+
+# Create post
+get "/posts/create" do
+  @title = "Create post"
+  @post = Post.new
+  erb :"posts/create"
+end
+post "/posts" do
+ @post = Post.new(params[:post])
+ if @post.save
+   redirect "posts/#{@post.id}", :notice => 'Congrats! Love the new post. (This message will disapear in 4 seconds.)'
+ else
+   redirect "posts/create", :error => 'Something went wrong. Try again. (This message will disapear in 4 seconds.)'
+ end
+end
+
+# view post
+get "/posts/:id" do
+  @post = Post.find(params[:id])
+  @title = @post.title
+  erb :"posts/view"
+end
+
+# edit post
+get "/posts/:id/edit" do
+  @post = Post.find(params[:id])
+  @title = "Edit Form"
+  erb :"posts/edit"
+end
+put "/posts/:id" do
+  @post = Post.find(params[:id])
+  @post.update(params[:post])
+  redirect "/posts/#{@post.id}"
 end
