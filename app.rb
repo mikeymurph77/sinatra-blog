96f4cf3cff35dc5b29b0ebc8677f1c8d82c1d634
Michael Murphy <mikeymurph77@gmail.com>
2016-01-09 15:27:39 -0500

    Can add and view post

--- app.rb
+++ app.rb
@@ -2,5 +2,45 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
 
+# Models
 class Post < ActiveRecord::Base
+end
+
+# Routes
+get "/" do
+  @posts = Post.order("created_at DESC")
+  @title = "Welcome."
+  erb :"posts/index"
+end
+
+get "/posts/create" do
+  @title = "Create post"
+  @post = Post.new
+  erb :"posts/create"
+end
+
+post "/posts" do
+  @post = Post.new(params[:post])
+  if @post.save
+    redirect "posts/#{@post.id}"
+  else
+    erb :"posts/create"
+  end
+end
+
+get "/posts/:id" do
+  @post = Post.find(params[:id])
+  @title = @post.title
+  erb :"posts/view"
+end
+
+# Helpers
+helpers do
+  def title
+    if @title
+      "#{@title}"
+    else
+      "Welcome."
+    end
+  end
 end
